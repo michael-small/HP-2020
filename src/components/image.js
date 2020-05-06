c55fec54e17ba70c5cb6836f42b0a6f53e8ff4c1
Task Ohmori <tascii.oh@gmail.com>
2020-05-07 02:43:52 +0900

    Github Action deploy

--- src/components/image.js
+++ src/components/image.js
@@ -28,7 +28,6 @@ export default ({ filename, className,  ...props }) => (
     `}
 
     render={(data) => {
-      console.log(data);
       const image = data.images.edges.find(n => {
         return n.node.relativePath.includes(filename)
       })
