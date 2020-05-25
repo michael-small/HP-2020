28c2ada51ad1ed944d71bca1e0e0a9987535b308
Task Ohmori <tascii.oh@gmail.com>
2020-05-25 14:00:37 +0900

    company/#teams background

--- src/components/company/team.jsx
+++ src/components/company/team.jsx
@@ -8,82 +8,87 @@ import { StyledLink as Link } from "../link"
 
 export default ({ ...props }) => {
   return (
-    <Container {...props}>
-      <h1>Teams</h1>
-      <FlexConteiner>
-        <FlexItem>
-          <Image
-            filename="1_-Sota-Watanabe_CEO.jpg"
-            style={{ width: 204, height: 204 }}
-          />
-          <Name>Sota Watanabe</Name>
-          <Job>CEO</Job>
-        </FlexItem>
-        <FlexItem>
-          <Image
-            filename="2_Takumi-Yamashita_CTO.jpg"
-            style={{ width: 204, height: 204 }}
-          />
-          <Name>Takumi Yamashita</Name>
-          <Job>CTO</Job>
-        </FlexItem>
-        <FlexItem>
-          <Image
-            filename="3_Masaharu-Uno_COO.jpg"
-            style={{ width: 204, height: 204 }}
-          />
-          <Name>Masaharu Uno</Name>
-          <Job>COO</Job>
-        </FlexItem>
-        <FlexItem>
-          <Image
-            filename="4_Task-Ohmori_Core-Developer.jpg"
-            style={{ width: 204, height: 204 }}
-          />
-          <Name>Task Ohmori</Name>
-          <Job>Core Developer</Job>
-        </FlexItem>
-        <FlexItem>
-          <Image
-            filename="5_-Alexandr-Krupenkin.png"
-            style={{ width: 204, height: 204 }}
-          />
-          <Name>Alexsandr Krupenkin</Name>
-          <Job>Software Engineer</Job>
-        </FlexItem>
-        <FlexItem>
-          <Image
-            filename="6_-Yoshinobu-Shijo.png"
-            style={{ width: 204, height: 204 }}
-          />
-          <Name>Yoshinobu Shijo</Name>
-          <Job>Product Manager</Job>
-        </FlexItem>
-        <FlexItem>
-          <Image
-            filename="7_Hoon-Kim_Software-Engineer.jpg"
-            style={{ width: 204, height: 204 }}
-          />
-          <Name>Hoon Kim</Name>
-          <Job>Software Engineer</Job>
-        </FlexItem>
-        <FlexItem>
-          <Image
-            filename="8_-Tomomasa-Matsunaga.png"
-            style={{ width: 204, height: 204 }}
-          />
-          <Name>Tomomasa Matsunaga</Name>
-          <Job>Software Engineer</Job>
-        </FlexItem>
-      </FlexConteiner>
-    </Container>
+    <Bg>
+      <Container {...props}>
+        <h1>Teams</h1>
+        <FlexConteiner>
+          <FlexItem>
+            <Image
+              filename="1_-Sota-Watanabe_CEO.jpg"
+              style={{ width: 204, height: 204 }}
+            />
+            <Name>Sota Watanabe</Name>
+            <Job>CEO</Job>
+          </FlexItem>
+          <FlexItem>
+            <Image
+              filename="2_Takumi-Yamashita_CTO.jpg"
+              style={{ width: 204, height: 204 }}
+            />
+            <Name>Takumi Yamashita</Name>
+            <Job>CTO</Job>
+          </FlexItem>
+          <FlexItem>
+            <Image
+              filename="3_Masaharu-Uno_COO.jpg"
+              style={{ width: 204, height: 204 }}
+            />
+            <Name>Masaharu Uno</Name>
+            <Job>COO</Job>
+          </FlexItem>
+          <FlexItem>
+            <Image
+              filename="4_Task-Ohmori_Core-Developer.jpg"
+              style={{ width: 204, height: 204 }}
+            />
+            <Name>Task Ohmori</Name>
+            <Job>Core Developer</Job>
+          </FlexItem>
+          <FlexItem>
+            <Image
+              filename="5_-Alexandr-Krupenkin.png"
+              style={{ width: 204, height: 204 }}
+            />
+            <Name>Alexsandr Krupenkin</Name>
+            <Job>Software Engineer</Job>
+          </FlexItem>
+          <FlexItem>
+            <Image
+              filename="6_-Yoshinobu-Shijo.png"
+              style={{ width: 204, height: 204 }}
+            />
+            <Name>Yoshinobu Shijo</Name>
+            <Job>Product Manager</Job>
+          </FlexItem>
+          <FlexItem>
+            <Image
+              filename="7_Hoon-Kim_Software-Engineer.jpg"
+              style={{ width: 204, height: 204 }}
+            />
+            <Name>Hoon Kim</Name>
+            <Job>Software Engineer</Job>
+          </FlexItem>
+          <FlexItem>
+            <Image
+              filename="8_-Tomomasa-Matsunaga.png"
+              style={{ width: 204, height: 204 }}
+            />
+            <Name>Tomomasa Matsunaga</Name>
+            <Job>Software Engineer</Job>
+          </FlexItem>
+        </FlexConteiner>
+      </Container>
+    </Bg>
   )
 }
+
+const Bg = styled.div`
+  background-color: #1a1a1a;
+`
 
 const Container = withTheme(styled(_Container)`
   ${localTheme}
   paddint-bottom: 116px;
-  background-color: #1a1a1a;
   letter-spacing: 0px;
   color: #ffffff;
   width: 100%;
