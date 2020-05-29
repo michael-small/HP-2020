3db49474c4a0a38808dd090ccad785d79b470514
Task Ohmori <tascii.oh@gmail.com>
2020-05-30 04:52:59 +0900

    fix font error

--- src/components/media/media-card.js
+++ src/components/media/media-card.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles(theme => ({
   },
   content: {
     "& h4": {
-      font: "15px/28px Noto Sans JP Bold",
+      font: "700 15px/28px Noto Sans JP",
     },
     "& p": {
       marginTop: "11px",
