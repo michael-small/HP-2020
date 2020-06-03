1db045a2e8f6dbf271dd219dc7d08ccda8e4f465
Task Ohmori <tascii.oh@gmail.com>
2020-06-04 03:58:39 +0900

    assets into source

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
       resolve: "gatsby-plugin-tinacms",
       options: {
         sidebar: {
